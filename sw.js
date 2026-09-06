8babbf3897888a5f6c849560ba284ef931da083f
xuvish <b6mf2jsdh2@privaterelay.appleid.com>
2026-09-06 10:19:12 +0530

    Make live cockpit visually match Widgy artwork

--- sw.js
+++ sw.js
@@ -1,5 +1,1 @@
-const CACHE='xuvish-radar-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('install',()=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
